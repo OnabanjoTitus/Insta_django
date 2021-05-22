951583f126a829c966089cafdea5aaf8f4c2986c
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-05-22 17:09:46 +0100

    updates

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from InstagramDjango import settings
-from .views import index
+from .views import index, NewImage
 
 urlpatterns = [
 
-    path('', index, name='homepage'),
+    path('', index, name='all'),
+    path('', NewImage, name='homepage'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- instagram/views.py
+++ instagram/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.forms import ModelForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from instagram.models import ImageUpload
 
 
 def index(request):
+    if request.method == 'POST':
+        form = NewImage(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('all')
     images = ImageUpload.objects.all()
+    form = NewImage()
     context = {
-        "images": images
+        "images": images,
+        "form": form
     }
     return render(request, "index.html", context)
+
+
+class NewImage(ModelForm):
+    class Meta:
+        model = ImageUpload
+        fields = "__all__"
